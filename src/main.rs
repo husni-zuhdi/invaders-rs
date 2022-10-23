52315c81b4d7466d89e27d0e1daf98846cddba0f
husnizuhdi <husni.zuhdi@tokopedia.com>
2022-10-23 13:01:17 +0700

    feat(audio): Add audios and test one of them

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use rusty_audio::Audio;
+
+fn main() -> Result <(), Box<dyn Error>>{
+    let mut audio = Audio::new();
+    audio.add("explode", "static/explode.wav");
+    audio.add("lose", "static/lose.wav");
+    audio.add("move", "static/move.wav");
+    audio.add("pew", "static/pew.wav");
+    audio.add("startup", "static/startup.wav");
+    audio.add("win", "static/win.wav");
+    audio.play("startup");
+
+    // Cleanup
+    audio.wait();
+    Ok(())
 }
